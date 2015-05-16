d362aaa1b48f191fa09ad7f51fe299f59bf2055b
zerdnem <zerdnem@gmail.com>
2015-05-16 18:21:15 +0800

    bible basher

--- install_fonts.sh
+++ install_fonts.sh
@@ -14,18 +14,28 @@ if [ "$dist" == "Arch Linux" ]; then
 
 	#Download fonts
 	if [ -d "$fontDir" ]; then
-		echo "Fonts already installed in "$fontDir""
+		echo "Fonts already installed in $fontDir"
 		exit 0
 	else
 		echo "Installing fonts"
 		svn export --force https://github.com/ryanoasis/nerd-filetype-glyphs-fonts-patcher/trunk/patched-fonts ~/.fonts/patched-fonts
+		mkfontscale "$fontDir"
+		mkfontdir "$fontDir"
 		#refresh fonts
-		sleep 5
-		echo "Refreshing font cache"
 		xset +fp "$fontDir"
 		xset fp rehash
-		fc-cache -vf
-		sleep 5
+		echo "Updating fontconfig font cache."
+
+		/usr/bin/fc-cache -f 2>/dev/null &
+		pid=$! # Process Id of the previous running command
+		spin='-\|/'
+		i=0
+		while kill -0 $pid 2>/dev/null
+		do
+			i=$(( (i+1) %4 ))
+			printf "\r${spin:$i:1}"
+			sleep .1
+		done
 	fi
 else
 	echo "Just install the fonts manually."
