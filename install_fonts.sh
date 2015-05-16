297a075cac1c8e37200e0b96b590315c0ea5df37
zerdnem <zerdnem@gmail.com>
2015-05-16 17:07:45 +0800

    bash me

--- install_fonts.sh
+++ install_fonts.sh
@@ -3,27 +3,30 @@ dist=$(gawk -F= '/^NAME/{print $2}' /etc/os-release | sed 's/"//g')
 fontDir="$HOME/.fonts/patched-fonts"
 
 if [ "$dist" == "Arch Linux" ]; then
-    set -e
-	echo "Installing subversion"
-
+	set -e
 	#Install subversion
-    sudo pacman --noconfirm -S subversion
-
+	if [ -x /usr/bin/svn ]; then
+		echo "subversion is already installed."
+	else
+		echo "Installing subversion"
+		sudo pacman --noconfirm -S subversion
+	fi
 
 	#Download fonts
-
-    if [ -d "$fontDir" ]; then
-        echo "Fonts already installed in ~/.fonts/patched-fonts"
-        exit 0
-    else
-        echo "Installing fonts"
-	    svn export --force https://github.com/ryanoasis/nerd-filetype-glyphs-fonts-patcher/trunk/patched-fonts ~/.fonts/patched-fonts
-	cd "$fontDir" && mkfontscale && mkfontdir
-	#refresh fonts
-	echo "Refreshing font cache"
-	xset +fp "$fontDir"
-	xset fp rehash
-    fi
+	if [ -d "$fontDir" ]; then
+		echo "Fonts already installed in "$fontDir""
+		exit 0
+	else
+		echo "Installing fonts"
+		svn export --force https://github.com/ryanoasis/nerd-filetype-glyphs-fonts-patcher/trunk/patched-fonts ~/.fonts/patched-fonts
+		cd "$fontDir" && mkfontscale && mkfontdir
+		#refresh fonts
+		sleep 1
+		echo "Refreshing font cache"
+		xset +fp "$fontDir"
+		xset fp rehash
+		sleep 5
+	fi
 else
 	echo "Just install the fonts manually."
 fi
